a88d98bc36620ede055d2d32a9b4481309f04dd1
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-04-10 21:44:39 +0200

    Inscription and Certificate model

--- backend/app/Models/Certificate.php
+++ backend/app/Models/Certificate.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Certificate extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'certificate_code',
+        'ending_date'
+    ];
+    // The attributes that should be casted
+    protected function casts(): array
+    {
+        return [
+            'ending_date' => 'datetime',
+        ];
+    }
+
+    // Define the relationships
+
+    // A cerificate belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    // A certificate belongs to a course
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+    // A method to give the format of the certificate date
+    public function getFormattedEndingDateAttribute()
+    {
+        return $this->ending_date?->format('d/m/Y');
+    }
+    public static function generateCode($userId, $courseId): string{
+        return 'CERT-'. strtoupper(uniqid()) . 'U' . $userId . 'C' . $courseId;
+    }
 }
--- backend/app/Models/Inscription.php
+++ backend/app/Models/Inscription.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inscription extends Model
 {
-    //
+    // Attributes that are mass assignable
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'inscription_date',
+    ];
+
+    // The attributes that should be casted
+    protected function casts(): array
+    {
+        return [
+            'inscription_date' => 'datetime',
+        ];
+    }
+
+    // Define the relationships
+
+    // An inscription belongs to a user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    // An inscription belongs to a course
+    public function course(){
+        return $this->belongsTo(Course::class);
+    }
+
+    // We add methods that can be reused 
+    public function scopeUsers($query, $userId){
+        return $query->where('user_id', $userId);
+    }
+    public function scopeCourse($query, $courseId){
+        return $query->where('course_id', $courseId);
+    }
 }
