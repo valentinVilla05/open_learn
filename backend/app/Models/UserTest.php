e686b666b5e1a42a6bb62f6761dbccfb1530dcf6
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-04-12 21:06:12 +0200

    Models finished

--- backend/app/Models/UserTest.php
+++ backend/app/Models/UserTest.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserTest extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'test_id',
+        'exam_id',
+        'user_answer',
+        'is_correct',   
+    ];
+    
+    // An answer belongs to an user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    // An answer belongs to a question
+    public function test(){
+        return $this->belongsTo(Test::class);
+    }
+    // An aswer belongs to an exam
+    public function exam(){
+        return $this->belongsTo(Exam::class);
+    }
+
+    // Method to get the answer of a question
+    public function checkAnswer(){
+        return $this->test->isCorrectAnswer($this->user_answer);
+    }
+    // Method to save the answer
+    public function setAnswer(){
+        $this->is_correct =  $this->checkAnswer();
+        $this->save();
+    }
 }
