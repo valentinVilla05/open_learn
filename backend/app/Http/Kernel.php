c48bc649c25aded4efe0cb106a75744eb74b5102
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-04-14 21:12:19 +0200

    Routes added, authentication in courses controller

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -26,11 +26,11 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
-
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/courses', [CourseController::class, 'showAllCourses']); // Everyone can see the courses
+Route::get('/courses/{id}', [CourseController::class, 'getCourseById']); // Everyone can an especific course
+
+// For creating, editing and deleting a course we need to be atohorized as an admin
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/courses', [CourseController::class, 'createCourse']);
+    Route::delete('/courses/{id}', [CourseController::class, 'deleteCourse']);
+    Route::put('/courses/{id}', [CourseController::class, 'updateCourse']);
+});
+
