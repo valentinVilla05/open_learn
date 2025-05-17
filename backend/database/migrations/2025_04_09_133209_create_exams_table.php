857b8f3c292ee3e31f6d551bec36544a5c1529de
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-05-17 20:30:38 +0200

    Exam migration changed

--- backend/database/migrations/2025_04_09_133209_create_exams_table.php
+++ backend/database/migrations/2025_04_09_133209_create_exams_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('course_id');
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->unique('course_id'); // Esta línea separada es más clara y expresiva
             $table->string('title')->nullable();
             $table->boolean('is_active')->default(false);
             $table->timestamps();
