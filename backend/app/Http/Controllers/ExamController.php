ecdb522c84700296a44ede5bc1c42d054783f968
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-05-17 20:21:59 +0200

    Exam restrixtions added (only one exam per course)

--- backend/app/Http/Controllers/ExamController.php
+++ backend/app/Http/Controllers/ExamController.php
@@ -7,28 +7,43 @@ use Illuminate\Http\Request;
 
 class ExamController extends Controller
 {
-    public function getAllExams(){
+    public function getAllExams()
+    {
         $exams = Exam::all();
 
         return response()->json($exams, 200);
     }
 
-    public function getExamsFromCourse($course_id){
+    public function getExamsFromCourse($course_id)
+    {
         $exams = Exam::where('course_id', $course_id)->get();
 
         return response()->json($exams, 200);
     }
 
-    public function createExam(Request $request){
+    public function createExam(Request $request)
+    {
         $data = $request->validate([
             'course_id' => 'required|integer',
             'title' => 'required|string',
-            'is_active' => 'boolean|default:false',
+            'is_active' => 'sometimes|boolean',
         ]);
+
+        $data['is_active'] = $data['is_active'] ?? false; // Set the filed false if not specified
+
+        // Check if there is any exam in that course
+        $existingExam = Exam::where('course_id', $data['course_id'])->first();
+        if ($existingExam) {
+            return response()->json([
+                'message' => 'An exam already exists for this course.'
+            ], 409); // 409 Conflict
+        }
+
         $examen = Exam::create($data);
         return response()->json(['message' => 'The exam was created succesfully'], 201);
     }
-    public function updateExam(Request $request, $id){
+    public function updateExam(Request $request, $id)
+    {
         $exam = Exam::findOrFail($id);
 
         $data = $request->validate([
@@ -39,7 +54,8 @@ class ExamController extends Controller
         return response()->json(['message' => 'The exam was updated succesfully'], 200);
     }
 
-    public function deleteExam($id){
+    public function deleteExam($id)
+    {
         $exam = Exam::findOrFail($id);
 
         $exam->delete();
