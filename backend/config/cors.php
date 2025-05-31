22c4b4c6c4a1346aec45619bee153f9ca047da86
valentinVilla05 <valentinvillapozo2005@gmail.com>
2025-06-01 01:29:07 +0200

    cors bug fixed

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://open-learn-daw.netlify.app/'],
+    'allowed_origins' => ['https://open-learn-daw.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
